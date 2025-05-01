6854054f742157ab1ffd022458d981233377b105
ahmadkafi74 <ahamdkafi@gmail.com>
2025-05-02 02:15:43 +0300

    Add migration file from backend branch

---
+++ database/migrations/2025_04_24_104736_create_complaints_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('complaints', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->enum('intity',['الكهرباء','المياه','البلدية','المالية','العقارية']);
+            $table->string('city');
+            $table->string('attachments')->nullable();
+            $table->text('description');
+            $table->boolean('is_emergency')->default(false);
+            $table->enum('status',['pending','accepted','rejected'])->default('pending');
+            $table->text('map_iframe')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('complaints');
+    }
+};
