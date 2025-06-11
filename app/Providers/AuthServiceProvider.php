e58ad2aef731fc85615f1fef36e2a576c3ee0cd9
ahmadkafi74 <ahamdkafi@gmail.com>
2025-06-11 17:19:05 +0300

    حل التعارضات مع develop

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -1,8 +1,10 @@
 <?php
 
 namespace App\Providers;
+
 use App\Models\Admin;
 use App\Models\Complaint;
+use App\Policies\AdminPolicy;
 use App\Models\CyberComplaint;
 use App\Policies\ComplaintsPolicy;
 use App\Policies\CyberComplaintsPolicy;
@@ -13,6 +15,7 @@ class AuthServiceProvider extends ServiceProvider
     protected $policies = [
         Complaint::class => ComplaintsPolicy::class,
         CyberComplaint::class => CyberComplaintsPolicy::class,
+        //Admin::class => AdminPolicy::class,
     ];
 
     /**
