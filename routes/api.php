ead7e2042b4a2194138ff82176509efb31e4a25f
Maher Tezeny <mahertezeny@gmail.com>
2025-06-11 11:21:45 +0000

    Edit api.php

--- routes/api.php
+++ routes/api.php
@@ -1,27 +1,53 @@
-
 <?php
 
-
-use App\Http\Controllers\Api\AuthController;
+// use App\Http\Controllers\Api\AuthController;
+use App\Models\ContactUs;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ContactUsController;
-use Illuminate\Http\Request;
 use App\Http\Controllers\SmartChatController;
-use App\Models\ContactUs;
+use App\Http\Controllers\UserComplaintController;
+use App\Http\Controllers\CyberComplaintController;
+use App\Http\Controllers\employee\auth\AuthController;
+use App\Http\Controllers\EmployeeComplaintsController;
+use App\Http\Controllers\EmployeeSuggestionController;
+use App\Http\Controllers\EmployeeCyberComplaintsController;
 
-// ChatBot Endpoint
+/* Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
+    return $request->user();
+}); */
+Route::get('/user', function (Request $request) {
+    return 'hello from api ';
+});
+
+########## ChatBot Endpoint
 Route::post('/chat', [SmartChatController::class, 'chat']);
 
-//ContactUs Endpoint
+######### ContactUs Endpoint
 Route::post('/contactus',[ContactUsController::class, '__invoke']);
-Route::controller(AuthController::class)->group(function(){
+######### Complaint Recource Endpoint
+Route::apiResource('User-Complaints',UserComplaintController::class)->only('index', 'store', 'show', 'destroy');
+######### CyberComplaint Endpoint
+Route::post('/cybercomplaint',[CyberComplaintController::class,'store']);
 
+Route::controller(AuthController::class)->group(function(){
+    
 });
-// Employee account
+######## Employee account
 Route::prefix('employee')->group(function () {
     Route::post('register', [AuthController::class, 'register']);
     Route::post('login', [AuthController::class, 'login']);
-    Route::post('logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+    Route::middleware('auth:sanctum')->group(function () {
+        #### middelware for making sure of logging in
+        Route::post('logout', [AuthController::class, 'logout']);
+        #### Get Complaints Gor Employee 
+        Route::get('complaints',[EmployeeComplaintsController::class,'getComplaints']);
+        Route::get('Suggestion',[EmployeeSuggestionController::class,'getSuggestions']);
+        Route::get('Cybercomplaints',[EmployeeCyberComplaintsController::class,'getComplaints']);
+        #### Update Status
+        Route::put('complaints/{id}/status',[EmployeeComplaintsController::class,'updateStatus']);
+    });
 });
 
 
@@ -29,5 +55,8 @@ Route::prefix('employee')->group(function () {
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
+
+    
+
 });
 
