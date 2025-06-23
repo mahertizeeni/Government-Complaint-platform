843410129ba99c0df6f6af3b85100d6b99fe6d19
Maher Tezeny <mahertezeny@gmail.com>
2025-06-23 14:59:10 +0000

    Edit web.php

--- routes/web.php
+++ routes/web.php
@@ -1,24 +1,27 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\Redis;
+// use Illuminate\Support\Facades\Redis;
 
-
-Route::get('/test-redis', function() {
-    try {
-        Redis::set('test_key', 'Hello Redis!');
-        return Redis::get('test_key');
-    } catch (\Exception $e) {
-        return 'Error: ' . $e->getMessage();
-    }
+Route::get('/',function (){
+dd('Welcome to Government Complaints Platform');
 });
 
+// Route::get('/test-redis', function() {
+//     try {
+//         Redis::set('test_key', 'Hello Redis!');
+//         return Redis::get('test_key');
+//     } catch (\Exception $e) {
+//         return 'Error: ' . $e->getMessage();
+//     }
+// });
 
 
-Route::get('/clear-chat', function () {
-    session()->forget('chat_history');
-    return 'تت تم مسح المحادثة القديمة.';
-});
+
+// Route::get('/clear-chat', function () {
+//     session()->forget('chat_history');
+//     return 'تت تم مسح المحادثة القديمة.';
+// });
 
 
 require __DIR__.'/auth.php';
