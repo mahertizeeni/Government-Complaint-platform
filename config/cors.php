3448479dd708389c70f38fab05e887fa0d80b8fb
maher <mahertezeny@gmail.com>
2025-06-30 21:56:08 +0300

    Cors

--- config/cors.php
+++ config/cors.php
@@ -6,19 +6,7 @@ return [
     |--------------------------------------------------------------------------
     | Cross-Origin Resource Sharing (CORS) Configuration
     |--------------------------------------------------------------------------
-    |
-    | Here you may configure your settings for cross-origin resource sharing
-    | or "CORS". This determines what cross-origin operations may execute
-    | in web browsers. You are free to adjust these settings as needed.
-    |
-    | To learn more: https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
-    |
     */
-
-
-
-
- 
 
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
@@ -32,7 +20,13 @@ return [
 
     'allowed_origins_patterns' => [],
 
-    'allowed_headers' => ['*'],
+    'allowed_headers' => [
+        'Content-Type',
+        'X-Requested-With',
+        'Authorization',
+        'Accept',
+        'Origin',
+    ],
 
     'exposed_headers' => [],
 
@@ -41,8 +35,3 @@ return [
     'supports_credentials' => true,
 
 ];
-
-
-
-
-
