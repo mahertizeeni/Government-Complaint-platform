98a3891e6631d4bd2bde72670891c4de40ba9d38
maher <mahertezeny@gmail.com>
2025-08-25 15:59:10 +0300

    cors

--- config/cors.php
+++ config/cors.php
@@ -4,7 +4,7 @@
 return [
 'paths' => ['api/*', 'sanctum/csrf-cookie', 'login', 'logout'],
 'allowed_methods' => ['*'],
-'allowed_origins' => ['http://localhost:5173','http://localhost:3000',],
+'allowed_origins' => ['http://localhost:5173','http://localhost:3000','https://gov-complaints-platform.onrender.com'],
 'allowed_origins_patterns' => [],
 'allowed_headers' => ['*'],
 'exposed_headers' => [],
